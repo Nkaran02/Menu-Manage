8dbd079999c5fb37e3c8a3996ba25e7ff2e688f3
Sujith N <karan820416@gmail.com>
2025-11-25 01:50:34 +0530

    little changes

--- backend/app/crud.py
+++ backend/app/crud.py
@@ -4,13 +4,6 @@ from . import models, schemas
 def get_dishes(db: Session):
     return db.query(models.Dish).all()
 
-# def toggle_dish_status(db: Session, dish_id: int):
-#     dish = db.query(models.Dish).filter(models.Dish.dishId == dish_id).first()
-#     if dish:
-#         dish.isPublished = not dish.isPublished
-#         db.commit()
-#         db.refresh(dish)
-#     return dish
 
 def toggle_dish_status(db: Session, dish_id: int):
     dish = db.query(models.Dish).filter(models.Dish.dishId == dish_id).first()
